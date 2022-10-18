dd5448317b561095fea219a7da789bf953c422e3
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2022-10-18 12:06:00 -0500

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+function cargar() {
+    let cont: number[] = [];
+    let promedio: number = 0;
+    series.forEach(function (serie) {
+        document.getElementById(serie.id+"-1")!.innerHTML = String(serie.id);
+        document.getElementById(serie.id+"-2")!.innerHTML = serie.name;
+        document.getElementById(serie.id+"-3")!.innerHTML = serie.channel;
+        document.getElementById(serie.id+"-4")!.innerHTML = String(serie.seasons);
+        cont.push(serie.seasons);
+    });
+    cont.forEach(function (cont) {
+        promedio += cont;
+    });
+    promedio = promedio / cont.length;
+    document.getElementById("promedio")!.innerHTML = "Seasons average: " + String(promedio);
+}
+
+window.onload = cargar;
